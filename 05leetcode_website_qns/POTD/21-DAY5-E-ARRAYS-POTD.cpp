50a1fa4b5922d44f6ee519c619179cc374f622c0
THARUN7474 <bandatharun74@gmail.com>
2025-07-05 15:01:33 +0530

    added potd

---
+++ 05leetcode_website_qns/POTD/21-DAY5-E-ARRAYS-POTD.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findLucky(vector<int> &arr)
+    {
+        unordered_map<int, int> freq;
+
+        for (int num : arr)
+        {
+            freq[num]++;
+        }
+
+        int lucky = -1;
+
+        for (auto &[key, value] : freq)
+        {
+            if (key == value)
+            {
+                lucky = max(lucky, key);
+            }
+        }
+
+        return lucky;
+    }
+};
